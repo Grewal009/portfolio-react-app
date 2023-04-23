b0eeff6e8e9056721b0f50428d22af6e3f5d507b
Aneel Grewal <aneel.grewal@hotmail.com>
2023-04-23 02:01:20 +0200

    home component created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import logo from "../assets/logo1.png";
-import {FaBars, FaTimes} from "react-icons/fa";
+import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from "react-icons/fa";
+import {HiOutlineMail} from "react-icons/hi"
+import {BsFillPersonLinesFill} from "react-icons/bs";
 const Navbar = () => {
 
     const [nav, setNav] = useState(false);
@@ -40,6 +42,32 @@ const Navbar = () => {
             <li className="py-6 text-4xl
             ">Contact</li>
         </ul>
+
+        {/**Social icons */}
+        <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+            <ul>
+                <li className="w-[140px] h-[60px] flex justify-center items-center ml-[-80px] hover:ml-[-10px] duration-300 bg-blue-600"> 
+                    <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                        Linkedin <FaLinkedin size={30}/>
+                    </a>
+                </li>
+                <li className="w-[140px] h-[60px] flex justify-center items-center ml-[-80px] hover:ml-[-10px] duration-300 bg-[#333333]"> 
+                    <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                        Github <FaGithub size={30}/>
+                    </a>
+                </li>
+                <li className="w-[140px] h-[60px] flex justify-center items-center ml-[-80px] hover:ml-[-10px] duration-300 bg-[#086634]"> 
+                    <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                        Email <HiOutlineMail size={30}/>
+                    </a>
+                </li>
+                <li className="w-[140px] h-[60px] flex justify-center items-center ml-[-80px] hover:ml-[-10px] duration-300 bg-[#565f69]"> 
+                    <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                        Resume <BsFillPersonLinesFill size={30}/>
+                    </a>
+                </li>
+            </ul>
+        </div>
 
     </div>
   )
