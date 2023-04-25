2f624cf39922fa19ba9e0a8a6d1b72983d6a5014
Aneel Grewal <aneel.grewal@hotmail.com>
2023-04-26 00:30:18 +0200

    Footer component created

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+import {BsFillSuitHeartFill} from "react-icons/bs"
+
+const Footer = () => {
+    return(
+        <div className="w-full h-16 bg-[#0a192f] text-gray-300 border-t-2 border-gray-300 flex justify-center" >
+            <p className="flex items-center">Made with &nbsp;<span className="text-pink-600"><BsFillSuitHeartFill /></span>&nbsp; in Oslo</p>
+
+        </div>
+    )
+}
+
+export default Footer;
