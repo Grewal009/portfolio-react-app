dc518c4b89b47e4dc60134beb64edfd60a9e2422
Aneel Grewal <aneel.grewal@hotmail.com>
2023-04-27 22:16:15 +0200

    favicon added & contact component changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
             <form method="POST" action="https://getform.io/f/a239643e-8c01-45c3-8a3e-e182456abb0e"  className="flex flex-col max-w-[600px] w-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 ">Contact</p>
-                    <p className="text-gray-300 py-4">Submit the form below or send me an email - aneel.grewal@hotmail.com</p>
+                    <p className="text-gray-300 py-4">Submit the form below.</p>
                 </div>
                 <input className="bg-[#ccd6f6] p-2" type="text" placeholder="Name" name="name" required/>
                 <input className="bg-[#ccd6f6] my-4 p-2" type="email" placeholder="Email" name="email" required/>
